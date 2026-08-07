5f5997e5cbf205514f501cc5b33cd861f097d5c0
Belotti17 <belottiwenze@gmail.com>
2026-08-07 08:31:56 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController; // Importe le Controller d'authentification
+use Illuminate\Http\Request; // Permet de récupérer la requête HTTP
+use Illuminate\Support\Facades\Route; // Permet de créer des routes API
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']); // Route API pour créer un utilisateur
+Route::post('/login', [AuthController::class, 'login']); // Route API pour connecter un utilisateur
+
+Route::get('/user', function (Request $request) { // Route pour récupérer l'utilisateur connecté
+    return $request->user(); // Retourne l'utilisateur authentifié
+})->middleware('auth:sanctum'); // Protège la route avec Sanctum
