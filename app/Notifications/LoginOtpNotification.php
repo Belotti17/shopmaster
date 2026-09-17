0e1b74d10d9cd41474be4834b4b099c5f953f317
Belotti17 <belottiwenze@gmail.com>
2026-09-17 14:47:20 +0100

    feat: create LoginOtpNotification class for sending OTP emails

---
+++ app/Notifications/LoginOtpNotification.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class LoginOtpNotification extends Notification
+{
+    public function __construct(private readonly string $code)
+    {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Code de connexion - ShopMaster')
+            ->greeting('Bonjour ' . $notifiable->name . ',')
+            ->line('Voici votre code de connexion :')
+            ->line($this->code)
+            ->line('Ce code est valable pendant 10 minutes.')
+            ->line('Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer cet email.');
+    }
+}
