ef7adfebcd97f5de444d7dc8d3380c63ac9ccdea
Belotti17 <belottiwenze@gmail.com>
2026-09-18 14:52:18 +0100

    feat: enhance login OTP email message for clarity and formatting

--- app/Notifications/LoginOtpNotification.php
+++ app/Notifications/LoginOtpNotification.php
@@ -21,9 +21,12 @@ class LoginOtpNotification extends Notification
         return (new MailMessage)
             ->subject('Code de connexion - ShopMaster')
             ->greeting('Bonjour ' . $notifiable->name . ',')
-            ->line('Voici votre code de connexion :')
-            ->line($this->code)
+            ->line('Voici votre code de connexion à votre compte ShopMaster.')
+            ->line('')
+            ->line('Code : ' . $this->code)
+            ->line('')
             ->line('Ce code est valable pendant 10 minutes.')
-            ->line('Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer cet email.');
+            ->line('Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer cet email.')
+            ->salutation('L’équipe ShopMaster');
     }
 }
